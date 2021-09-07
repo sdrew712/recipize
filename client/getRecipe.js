809e571a0da3594a08a96777cf079fe58e3a8a44
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-07 00:45:29 -0700

    rendered all recipe contents

--- client/getRecipe.js
+++ client/getRecipe.js
@@ -1,9 +1,13 @@
 const getRecipeButton = document.getElementById("get-recipe")
 const newRecipeContainer = document.getElementById("new-recipe-container")
-const recipeTitle = document.getElementById("recipe-title")
-const origin = document.getElementById("origin")
-const category = document.getElementById("category")
-const ingredients = document.getElementById("ingredients")
+const bannerElement = document.getElementById("banner")
+const recipeTitleElement = document.getElementById("recipe-title")
+const originElement = document.getElementById("origin")
+const categoryElement = document.getElementById("category")
+const tagsElement = document.getElementById("tags")
+const ingredientsElement = document.getElementById("ingredients")
+const instructionsElement = document.getElementById("instructions")
+const youtubeEmbedElement = document.getElementById("youtube-embed")
 
 getRecipeButton.addEventListener('click', displayRandomRecipe)
 
@@ -11,13 +15,32 @@ async function displayRandomRecipe(){
   let response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
     .then(res => res.json())
     .catch(console.error);
-
-  console.log(response.meals[0]);
-
+  
   const recipe = response.meals[0];
+  console.log({ recipe });
 
-  recipeTitle.innerHTML = recipe.strMeal
-  origin.innerHTML = recipe.strArea
-  category.innerHTML = recipe.strCategory
-  ingredients.innerHTML = recipe.strIngredient1
+  const ingredients = [];
+
+  recipeTitleElement.innerHTML = recipe.strMeal
+  originElement.innerHTML = recipe.strArea
+  categoryElement.innerHTML = recipe.strCategory
+  tagsElement.innerHTML = recipe.strTags
+
+  for (let i = 1; i <= 20; i++) {
+    const ingredient = recipe["strIngredient".concat(i)];
+    const measurement = recipe["strMeasure".concat(i)];
+
+    if (ingredient) {
+      ingredients.push(measurement + " " + ingredient);
+    }
+  
+  }
+
+  ingredientsElement.innerHTML = ingredients;
+  // console.log({ ingredients })
+  instructionsElement.innerHTML = recipe.strInstructions
+  
+  bannerElement.style.backgroundImage = `url('${recipe.strMealThumb}')`
+
+  youtubeEmbedElement.src = recipe.strYoutube.replace("watch?v=", "embed/")
 }
