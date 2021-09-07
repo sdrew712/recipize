5b2d480ab2c54017f095e9e46565215a2d5f6d5f
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-07 16:09:38 -0700

    undid deployment changes

--- server/index.js
+++ server/index.js
@@ -1,29 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const controller = require("./controller")
-const path = require("path");
 
 const app = express();
-
 app.use(cors());
+
 app.use(express.json());
-
-app.use('/css', express.static(path.join(__dirname, '../client/styles.css')))
-
-app.use('/js', express.static(path.join(__dirname, '../client/main.js')))
-
-app.use('/getRecipe', express.static(path.join(__dirname, '../client/getRecipe.js')))
-
-app.get("/", function(req, res){
-  res.sendFile(path.join(__dirname, "../client/index.html"));
-});
 
 app.post("/api/recipe", controller.displayRecipe)
 
 app.delete("/api/recipe/:id", controller.deleteRecipe)
 
-const port = process.env.PORT || 4005;
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-})
+app.listen(4000, () => console.log("Server running on 4000"));
