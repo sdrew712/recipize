160e87db74b5e0121cb0e4ace9c534d736909159
Sarah Drew <drew.sarah.e@gmail.com>
2021-09-07 15:59:01 -0700

    linked css and js, added procfile

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const cors = require("cors");
 const controller = require("./controller")
+const path = require("path");
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 app.use(express.json());
+
+app.use('/css', express.static(path.join(__dirname, '../client/styles.css')))
+
+app.use('/js', express.static(path.join(__dirname, '../client/main.js')))
+
+app.use('/getRecipe', express.static(path.join(__dirname, '../client/getRecipe.js')))
 
 app.post("/api/recipe", controller.displayRecipe)
 
